7958885d0e22a5f93adaf1493ad097bba5dd1026
itvtt <itvtt1@gmail.com>
2024-11-15 12:25:33 +0900

    up

--- app.js
+++ app.js
@@ -9,13 +9,21 @@ const app = express();
 // const router = express.Router();
 
 app.use(express.json());
+
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-// app.use([loginRouter,postsRouter,commentsRouter,likeRouter]);
-
-app.listen(8080, () => {
-  console.log("서버가 요청을 받을 준비가 됐어요");
-  console.log("gd")
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
+
+// // app.use([loginRouter,postsRouter,commentsRouter,likeRouter]);
+
+// app.listen(8080, () => {
+//   console.log("서버가 요청을 받을 준비가 됐어요");
+//   console.log("gd")
+// });
